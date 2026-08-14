392d2dd2cfbce5d7e666127b738451cc440f6bba
chloridesnail <ayron.herath.uk@gmail.com>
2026-08-14 01:52:23 +0100

    added new functions

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ class Evaluate:
             line = lines[pc]
             match line.split(maxsplit = 1)[0]:
                 case "while":
-                    if self.execute(line.split(maxsplit = 1)[1]) == 1: pc += 1#if the condition is true
+                    if self.execute(line.split(maxsplit = 1)[1]) == 1: pc += 1 #if the condition is true
                     else:
                         while lines[pc].split(maxsplit = 1)[0] != "end": pc += 1
                         pc += 1
                 case "end":
                     while lines[pc].split(maxsplit = 1)[0] != "while": pc -= 1
+                case "print":
+                    (_, expr) = line.split(maxsplit = 1)
+                    if expr in self.vars:
+                        print(self.vars[expr])
+                    else:
+                        print(expr)
+                    pc += 1
                 case _:
                     (name, _, expr) = line.split(maxsplit = 2) #splits line into 3 parts: var name, ignores the equals, rest of the line
                     self.vars[name] = self.execute(expr)
                     pc += 1
-        print(self.vars)
     def execute(self, s):
         tokens = s.split()
         stack = [] # Reverse Polish Notation stack
@@ -29,14 +35,39 @@ class Evaluate:
             elif token in self.vars: stack.append(self.vars[token])
             else:
                 rhs = stack.pop()
-                lhs = stack.pop()
-                if token == "+": stack.append(lhs + rhs)
-                elif token == "-": stack.append(lhs - rhs)
-                elif token == "/": stack.append(lhs / rhs)
-                elif token == "*": stack.append(lhs * rhs)
-                elif token == ">=":
-                    if lhs >= rhs: stack.append(1)
-                    else: stack.append(0) 
+                if token == "not":
+                    if rhs == 1: stack.append(0)
+                    else: stack.append(1)
+                else:
+                    lhs = stack.pop()
+                    if token == "+": stack.append(lhs + rhs)
+                    elif token == "-": stack.append(lhs - rhs)
+                    elif token == "/": stack.append(lhs / rhs)
+                    elif token == "*": stack.append(lhs * rhs)
+                    elif token == ">=":
+                        if lhs >= rhs: stack.append(1)
+                        else: stack.append(0) 
+                    elif token == "<=":
+                        if lhs <= rhs: stack.append(1)
+                        else: stack.append(0)
+                    elif token == ">":
+                        if lhs > rhs: stack.append(1)
+                        else: stack.append(0)
+                    elif token == "<":
+                        if lhs < rhs: stack.append(1)
+                        else: stack.append(0)
+                    elif token == "==":
+                        if lhs == rhs: stack.append(1)
+                        else: stack.append(0)
+                    elif token == "!=":
+                        if lhs != rhs: stack.append(1)
+                        else: stack.append(0)
+                    elif token == "and":
+                        if lhs == 1 and rhs == 1: stack.append(1)
+                        else: stack.append(0)
+                    elif token == "or":
+                        if lhs == 1 or rhs == 1: stack.append(1)
+                        else: stack.append(0)
         return stack[0]
 
 Evaluate().lineReader(open(sys.argv[1]).read()) #python main.py [text file (code)]
